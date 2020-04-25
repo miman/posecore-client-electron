c9dabf13e1712b1be621c93e5c9ed26da52dcbb0
Mikael <mikael.thorman@gmail.com>
2020-04-25 08:33:18 +0200

    Improved web page styling & information

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ function createWindow () {
     height: 600,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    icon: __dirname + '/posecore.png'
   })
 
   // and load the index.html of the app.
